8a6bf31b61f8bdf8618ed9194d920ab768dc0d1e
Zii Haruno <54045609+ziiharuno@users.noreply.github.com>
2021-08-27 15:10:51 -0300

    Rename 005.py to AntecessoreSucessor.py

--- 005.py
+++
@@ -1,5 +0,0 @@
-n= int(input("Digite um valor: "))
-a= n+1
-b= n-1
-
-print('O número sucessor de {} é {}, e o antecessor é {}'.format(n,a,b))
---
+++ AntecessoreSucessor.py
@@ -0,0 +1,5 @@
+n= int(input("Digite um valor: "))
+a= n+1
+b= n-1
+
+print('O número sucessor de {} é {}, e o antecessor é {}'.format(n,a,b))
